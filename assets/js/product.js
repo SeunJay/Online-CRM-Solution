76c1fe86ace55138f427d6e127805a1b4d7f7e91
penance <egbouchenna001@gmail.com>
2019-03-15 09:34:18 +0100

    add modal

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,16 +1,16 @@
-$(function() {
+$(function () {
     // var db = low('db.json');
-    $.fn.serializeObject = function() {
+    $.fn.serializeObject = function () {
         var o = {};
         var a = this.serializeArray();
-        $.each(a, function() {
+        $.each(a, function () {
             if (o[this.name]) {
                 if (!o[this.name].push) {
                     o[this.name] = [o[this.name]];
                 }
-                o[this.name].push(this.value || '');
+                o[this.name].push(this.value || "");
             } else {
-                o[this.name] = this.value || '';
+                o[this.name] = this.value || "";
             }
         });
         return o;
@@ -18,89 +18,84 @@ $(function() {
 
     /********************** PRODUCT FUNCTIONALITY  *******************************/
 
-    // displaying form for adding products
-    $("#addProduct").click(function(event) {
-        event.preventDefault();
-        $('.fWrapper').animate({
-            height: "toggle",
-            opacity: "toggle"
-        }, "slow")
-        $('.fWrapper').toggleClass('hidden');
-        $('.EWrapper').hide();
-    })
-
-
     // creating new product on product form
-
-    $('form#productForm').on('submit', function(e) {
+    $("form#productForm").on("submit", function (e) {
         e.preventDefault();
 
         var formData = $(this).serializeObject();
         console.log(JSON.stringify(formData));
 
         $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/products',
+            type: "POST",
+            url: "http://localhost:3000/products",
             data: JSON.stringify(formData),
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                Accept: "application/json",
+                "Content-Type": "application/json"
             },
-
-            success: function(data) {
-                location.reload();
-                $('.fWrapper').animate({
-                    height: "toggle",
-                    opacity: "toggle"
-                }, "slow")
-                $("#clearProductForm").trigger('click');
+            success: function (data) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 1500,
+                    onClose: function () {
+                        window.location.reload();
+                    }
+                });
+                Toast.fire({
+                    type: "success",
+                    title: "Added successfully"
+                });
             },
-            error: function(err) {
+            error: function (err) {
                 console.log(err);
             }
-        })
-    })
+        });
+    });
 
-    $("#clearProductForm").click(function(e){
-        e.preventDefault()
+    // clear product form Upon clicking
+    $("#clearProductForm").click(function (e) {
+        e.preventDefault();
         $("#productForm")[0].reset();
-    })
+    });
 
-
-
-     // displaying all created products 
-        
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:3000/products',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-
-            
-            // function to enumerate created products
-
-        }).done(function(data) {
-            var $tableBody = $('tbody');
+    // displaying all created products
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/products",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        var $tableBody = $("tbody");
         $tableBody.empty();
 
-        
-        if(data.length) {
+        // function to enumerate created products
+        if (data.length) {
             data.forEach(item => {
-
-            var $row = $('<tr>')
-                var $productId = $('<td>');
-                var $productName = $('<td>');
-                var $productQty = $('<td>');
-                var $productPrice = $('<td>');
-                var $editAction = $(`<td><button id="editBtn" class="btn btn-outline-primary" onClick="editProduct(${item.id})">Edit Product</button></td>`)
-                var $delAction = $(`<td><button id="delBtn" class="btn btn-outline-danger" onClick="deleteProduct(${item.id})">Delete Product</button></td>`)
-                $productId.append(item.id +' .');
+                var $row = $("<tr>");
+                var $productId = $("<td>");
+                var $productName = $("<td>");
+                var $productQty = $("<td>");
+                var $productPrice = $("<td>");
+                var $editAction = $(
+                    `<td><button id="editBtn" data-toggle="modal" data-target="#editProduct" class="btn btn-outline-primary" onClick="editProduct(${
+                    item.id
+                    })">Edit</button></td>`
+                );
+                var $delAction = $(
+                    `<td><button id="delBtn" class="btn btn-outline-danger" onClick="deleteProduct(${
+                    item.id
+                    })">Delete</button></td>`
+                );
+                $productId.append(item.id + " .");
                 $productName.append(item.productName);
                 $productQty.append(item.productQty);
-                $productPrice.append(item.productPrice)
-                $row.append($productId)
+                $productPrice.append(item.productPrice);
+                $row
+                    .append($productId)
                     .append($productName)
                     .append($productQty)
                     .append($productPrice)
@@ -108,101 +103,91 @@ $(function() {
                     .append($delAction)
                     .appendTo($tableBody);
             });
-    
         } else {
-            $tableBody.append('No product added yet');     
+            $tableBody.append("No product added yet");
         }
-        });
-    // })
+    });
 
-
-     
-    // function displayProductResult(data) { 
-        
-
-        
-    // }
-
-
-    // editing a product
-    $('form#productEditForm').on('submit', function(e) {
-        e.preventDefault();
-        var formData = $(this).serializeObject();
-
-        $.ajax({
-            type: 'PUT',
-            url: 'http://localhost:3000/products/' + formData.id,
-            data: JSON.stringify(formData),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-
-            success: function(data) {
-                $('.EWrapper').toggleClass('hidden');
-            },
-            error: function(err) {
-                console.log(err);
-            }
-        })
-    })
-
-    
     // clear edit form upon clicking
-    $("#clearProductEditForm").click(function(e){
-        e.preventDefault()
+    $("#clearProductEditForm").click(function (e) {
+        e.preventDefault();
         $("#productEditForm")[0].reset();
-    })
+    });
+});
 
-
-    // deleting a product using his id
-    
-    
-})
-
-function deleteProduct(id) {
+// editing a product using using its id
+function editProduct(id) {
     $.ajax({
-        method: 'DELETE',
+        method: "GET",
         url: `http://localhost:3000/products/${id}`,
-        success: function() {
-            console.log('success');
-            location.reload();
-            $("#getProduct").trigger('click');
+        success: function (data) {
+            $.each(data, function (key, value) {
+                $("[name=" + key + "]", "#productEditForm").val(value);
+            });
         },
         error: function (err) {
             console.log(err);
         }
-    })
-}
+    });
 
-    // editing a product using using its id
-    
-    function editProduct(id) {
-        $('.EWrapper').animate({
-            height: "toggle",
-            opacity: "toggle"
-        }, "slow")
-        $('.EWrapper').toggleClass('hidden');
-        $('.fWrapper').hide();
+    // editing a product
+    $("form#productEditForm").on("submit", function (e) {
+        e.preventDefault();
+        var formData = $(this).serializeObject();
 
         $.ajax({
-            method: 'GET',
-            url: `http://localhost:3000/products/${id}`, // returns undefined and doesnt put to db 
-            success: function(data) {
-            console.log(data);
-            $.each(data, function(key, value){
-                $('[name='+key+']', '#productEditForm').val(value);
-            });
+            type: "PUT",
+            url: `http://localhost:3000/products/${id}`,
+            data: JSON.stringify(formData),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+
+            success: function (data) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 1500,
+                    onClose: function () {
+                        window.location.reload();
+                    }
+                });
+                Toast.fire({
+                    type: "success",
+                    title: "Updated successfully"
+                });
             },
             error: function (err) {
                 console.log(err);
             }
-        })
-    }
+        });
+    });
+}
 
-
-
-
-
-
-
+// deleting a product using its id
+function deleteProduct(id) {
+    $.ajax({
+        method: "DELETE",
+        url: `http://localhost:3000/products/${id}`,
+        success: function () {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 1500,
+                onClose: function () {
+                    window.location.reload();
+                }
+            });
+            Toast.fire({
+                type: "success",
+                title: "Deleted successfully"
+            });
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
--- assets/js/sales.js
+++ assets/js/sales.js
@@ -1,196 +1,199 @@
-        $(function() {
-            $.fn.serializeObject = function() {
-                var o = {};
-                var a = this.serializeArray();
-                $.each(a, function() {
-                    if (o[this.name]) {
-                        if (!o[this.name].push) {
-                            o[this.name] = [o[this.name]];
-                        }
-                        o[this.name].push(this.value || '');
-                    } else {
-                        o[this.name] = this.value || '';
-                    }
-                });
-                return o;
-            };
-
-            /********************** SALES FUNCTIONALITY*******************************/
-
-            // displaying form for adding sales
-            $("#addSales").click(function(event){
-                event.preventDefault();
-                $('.fWrapper').animate({
-                    height: "toggle",
-                    opacity: "toggle"
-                }, "slow") 
-                $('.fWrapper').toggleClass('hidden');
-                $('.EWrapper').hide();
-                
-            })
-
-
-            // creating new sales on sales form
-            $('form#salesForm').on('submit', function(e) {
-                e.preventDefault();
-
-                var formData = $(this).serializeObject();
-                console.log(JSON.stringify(formData));
-
-                $.ajax({
-                    type: 'POST',
-                    url: 'http://localhost:3000/sales',
-                    data: JSON.stringify(formData),
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-
-                    success: function(data) {
-                        location.reload();
-                        $('.fWrapper').animate({
-                            height: "toggle",
-                            opacity: "toggle"
-                        }, "slow")
-                        $("#clearUserForm").trigger('click');
-                    },
-                    error: function(err) {
-                        console.log(err);
-                    }
-                })
-            })
-
-                    // clear form upon clicking
-                    $("#clearSalesForm").click(function(e){
-                        e.preventDefault()
-                        $("#salesForm")[0].reset();
-                    })
-
-
-
-            // displaying all created sales 
-
-                
-                $.ajax({
-                    type: 'GET',
-                    url: 'http://localhost:3000/sales',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                
-            }).done(function(data) {
-                var $tableBody = $('tbody');
-                $tableBody.empty();
-
-        
-                // function to enumerate created sales
-
-            if(data.length) {
-                data.forEach(item => {
-                    var $row = $('<tr>')
-
-                    var $salesId = $('<td>')
-                    var $custname = $('<td>');
-                    var $product = $('<td>');
-                    var $quantity = $('<td>');
-                    var $price = $('<td>');
-                    var $editAction = $(`<td><button id="editBtn" class="btn btn-outline-primary" onClick="editSales(${item.id})">Edit Sales</button></td>`)
-                    var $delAction = $(`<td><button id="delBtn" class="btn btn-outline-danger" onClick="deleteSales(${item.id})">Delete Sales</button></td>`)
-                    $salesId.append(item.id +' .');
-                    $custname.append(item.custname);
-                    $product.append(item.product);
-                    $quantity.append(item.quantity)
-                    $price.append(item.price)
-                    $row.append($salesId)
-                        .append($custname)
-                        .append($product)
-                        .append($quantity)
-                        .append($price)
-                        .append($editAction)
-                        .append($delAction)
-                        .appendTo($tableBody);
-                });
-
+$(function () {
+    $.fn.serializeObject = function () {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || "");
             } else {
-                $tableBody.append('No user added yet');    
+                o[this.name] = this.value || "";
             }
-            });
-
-            // clear edit form upon clicking
-            $("#clearSalesEditForm").click(function(e){
-                e.preventDefault()
-                $("#salesEditForm")[0].reset();
-            })
-
-            
-
-
-            // editing a sales
-            $('form#salesEditForm').on('submit', function(e) {
-                e.preventDefault();
-                var formData = $(this).serializeObject();
-
-                $.ajax({
-                    type: 'PUT',
-                    url: 'http://localhost:3000/sales/' + formData.id,
-                    data: JSON.stringify(formData),
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-
-                    success: function(data) {
-                        $('.EWrapper').toggleClass('hidden');
-                    },
-                    error: function(err) {
-                        console.log(err);
-                    }
-                })
-            })
-        
-
         });
+        return o;
+    };
 
+    /********************** SALES FUNCTIONALITY*******************************/
 
-        // deleting a sales using his id
-        function deleteSales(id) {
-            $.ajax({
-                method: 'DELETE',
-                url: `http://localhost:3000/sales/${id}`,
-                success: function() {
-                    console.log('success');
-                    location.reload();
-                    $("#getSales").trigger('click');
-                },
-                error: function (err) {
-                    console.log(err);
-                }
-            })
+    // creating new sales on sales form
+    $("form#salesForm").on("submit", function (e) {
+        e.preventDefault();
+
+        var formData = $(this).serializeObject();
+        console.log(JSON.stringify(formData));
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/sales",
+            data: JSON.stringify(formData),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+
+            success: function (data) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 2000,
+                    onClose: function () {
+                        window.location.reload();
+                    }
+                });
+                Toast.fire({
+                    type: "success",
+                    title: "Added successfully"
+                });
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    });
+
+    // clear form upon clicking
+    $("#clearSalesForm").click(function (e) {
+        e.preventDefault();
+        $("#salesForm")[0].reset();
+    });
+
+    // displaying all created sales
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/sales",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
         }
+    }).done(function (data) {
+        var $tableBody = $("tbody");
+        $tableBody.empty();
 
+        // function to enumerate created sales
+        if (data.length) {
+            data.forEach(item => {
+                var $row = $("<tr>");
 
+                var $salesId = $("<td>");
+                var $custname = $("<td>");
+                var $product = $("<td>");
+                var $quantity = $("<td>");
+                var $price = $("<td>");
+                var $editAction = $(
+                    `<td><button id="editBtn" data-toggle="modal" data-target="#editSales" class="btn btn-outline-primary" onClick="editSales(${
+                    item.id
+                    })">Edit</button></td>`
+                );
+                var $delAction = $(
+                    `<td><button id="delBtn" class="btn btn-outline-danger" onClick="deleteSales(${
+                    item.id
+                    })">Delete</button></td>`
+                );
+                $salesId.append(item.id + " .");
+                $custname.append(item.custname);
+                $product.append(item.product);
+                $quantity.append(item.quantity);
+                $price.append(item.price);
+                $row
+                    .append($salesId)
+                    .append($custname)
+                    .append($product)
+                    .append($quantity)
+                    .append($price)
+                    .append($editAction)
+                    .append($delAction)
+                    .appendTo($tableBody);
+            });
+        } else {
+            $tableBody.append("No user added yet");
+        }
+    });
 
-                // editing a sales using using his id
-                function editSales(id) {
-                    $('.EWrapper').animate({
-                        height: "toggle",
-                        opacity: "toggle"
-                    }, "slow")
-                    $('.EWrapper').toggleClass('hidden');
-                    $('.fWrapper').hide();
-                    
-                    $.ajax({
-                        method: 'GET',
-                        url: `http://localhost:3000/sales/${id}`,
-                        success: function(data) {
-                        console.log(data);
-                        $.each(data, function(key, value){
-                            $('[name='+key+']', '#salesEditForm').val(value);
-                        });
-                        },
-                        error: function (err) {
-                            console.log(err);
-                        }
-                    })
+    // clear edit form upon clicking
+    $("#clearSalesEditForm").click(function (e) {
+        e.preventDefault();
+        $("#salesEditForm")[0].reset();
+    });
+});
+
+// deleting a sales using his id
+function deleteSales(id) {
+    $.ajax({
+        method: "DELETE",
+        url: `http://localhost:3000/sales/${id}`,
+        success: function () {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 2000,
+                onClose: function () {
+                    window.location.reload();
                 }
+            });
+            Toast.fire({
+                type: "success",
+                title: "Deleted successfully"
+            });
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+// editing a sales using its id
+function editSales(id) {
+    $.ajax({
+        method: "GET",
+        url: `http://localhost:3000/sales/${id}`,
+        success: function (data) {
+            console.log(data);
+            $.each(data, function (key, value) {
+                $("[name=" + key + "]", "#salesEditForm").val(value);
+            });
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+
+    // editing a sales
+    $("form#salesEditForm").on("submit", function (e) {
+        e.preventDefault();
+        var formData = $(this).serializeObject();
+
+        $.ajax({
+            type: "PUT",
+            url: `http://localhost:3000/sales/${id}`,
+            data: JSON.stringify(formData),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+
+            success: function (data) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 2000,
+                    onClose: function () {
+                        window.location.reload();
+                    }
+                });
+                Toast.fire({
+                    type: "success",
+                    title: "Updated successfully"
+                });
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    });
+}
